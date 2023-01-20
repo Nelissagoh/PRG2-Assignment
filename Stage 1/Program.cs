b959c67cf970df46fa00d81fef4fb85277b56e06
s10244321 <s10244321f@connect.np.edu.sg>
2023-01-21 06:07:03 +0800

    first feature completed

--- Stage 1/Program.cs
+++ Stage 1/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Stage_1;
+using System.ComponentModel.DataAnnotations;
+
+void DisplayAllGuest()
+{
+    string[] lines = File.ReadAllLines("Guests.csv");
+    for (int i = 0; i < lines.Length; i++)
+    {
+        string[] info = lines[i].Split(',');
+        Console.WriteLine("{0,-10}{1,-20}{2,-20}{3,-20}", info[0], info[1], info[2], info[3]);
+    }
+    
+
+}
+
+DisplayAllGuest();
+
+
